c71dcf6420ac07bb01967572bcdf1f51ea4a50fb
Jelena Stojsin <ukradenitrenutak@gmail.com>
2018-08-24 09:10:58 +0200

    Zadatak 1

---
+++ src/components/NavBar.vue
@@ -0,0 +1,27 @@
+<template>
+     <nav class="navbar navbar-inverse bg-dark">
+        <div class="container-fluid">
+            <div class="navbar-header">
+            <a class="navbar-brand" href="/">Trivia</a>
+            </div>
+            <ul class="nav navbar-nav">
+                <a href="/">Home</a>            
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+            </ul>
+        </div>
+    </nav> 
+    
+</template>
+
+<script>
+export default {
+    name: 'navbar'
+}
+</script>
+
+<style>
+
+</style>
